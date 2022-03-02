fb9f0be31414a264b3a8c05d2e7c754a7fdae46a
Ivan Markovski <sagitariusim@gmail.com>
2022-03-02 13:06:10 +0100

    For review

---
+++ Week_3_Lecture_Task/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Card struct {
+	face  string
+	suit  string
+	cards *Card // next
+}
+
+type DeckOfCards struct {
+	deck *Card // head
+}
+
+var faces = [13]string{"Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+	"Queen", "King", "Ace"}
+var suits = [4]string{"Clubs", "Diamonds", "Hearts", "Spades"}
+
+func InitializeDeck(dc *[52]DeckOfCards) {
+
+	for i := range dc {
+		dc[i].deck = &Card{face: faces[i%13], suit: suits[i/13]}
+		fmt.Print(dc[i].deck)
+
+		if i%4 == 0 && i > 0 {
+			fmt.Println()
+
+		}
+	}
+}
+func main() {
+
+	cardDeck := &[52]DeckOfCards{}
+	InitializeDeck(cardDeck)
+}
