82483652c2a7be1b82bbf3bbc9bfddbf4d868f61
Ivan Markovski <sagitariusim@gmail.com>
2022-03-03 22:20:39 +0100

    For review

--- Week_3_Lecture_7_Task_2/main.go
+++ Week_3_Lecture_7_Task_2/main.go
@@ -5,20 +5,19 @@ import (
 )
 
 type Card struct {
-	face  string
-	suit  string
-	cards *Card
+	face string
+	suit string
 }
 
 type DeckOfCards struct {
-	deck *Card
+	deck Card
 }
 
 var faces = [13]string{"Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
 	"Queen", "King", "Ace"}
 var suits = [4]string{"Clubs", "Diamonds", "Hearts", "Spades"}
 
-func compareCards(firstCard *Card, secondCard *Card, deck *[52]DeckOfCards) {
+func compareCards(deck []DeckOfCards) {
 
 	var faceOfCard1 string
 	var suitOfCard1 string
@@ -26,102 +25,102 @@ func compareCards(firstCard *Card, secondCard *Card, deck *[52]DeckOfCards) {
 	var suitOfCard2 string
 	fmt.Print("Please enter face of the card ", 1, " (Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten,Jack, Queen, King, Ace) : ")
 	fmt.Scan(&faceOfCard1)
-	//firstCard.face = faceOfCard1
 
 	fmt.Print("Please enter suit of the card ", 1, " (Clubs, Diamonds, Hearts, Spades) : ")
 	fmt.Scan(&suitOfCard1)
-	//firstCard.suit = suitOfCard1
 
 	fmt.Print("Please enter face of the card ", 2, " (Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten,Jack, Queen, King, Ace) : ")
 	fmt.Scan(&faceOfCard2)
-	//secondCard.face = faceOfCard2
 
 	fmt.Print("Please enter suit of the card ", 2, " (Clubs, Diamonds, Hearts, Spades) : ")
 	fmt.Scan(&suitOfCard2)
-	//secondCard.suit = suitOfCard2
 
-	var faceIndex1 *int
-	var faceIndex2 *int
+	var faceIndex1 int
+	var faceIndex2 int
 
 	for i := 0; i < len(deck); i++ {
 		if faceOfCard1 == deck[i].deck.face {
-			faceIndex1 = &i
+			faceIndex1 = i
 		}
 	}
 
 	for i := 0; i < len(deck); i++ {
 		if faceOfCard2 == deck[i].deck.face {
-			faceIndex2 = &i
+			faceIndex2 = i
 		}
 	}
 
-	var suitIndex1 *int
-	var suitIndex2 *int
+	var suitIndex1 int
+	var suitIndex2 int
 
 	for i := 0; i < len(deck); i++ {
 
 		if suitOfCard1 == deck[i].deck.suit {
-			suitIndex1 = &i
+			suitIndex1 = i
 		}
 	}
 
 	for i := 0; i < len(deck); i++ {
 		if suitOfCard2 == deck[i].deck.suit {
 
-			suitIndex2 = &i
+			suitIndex2 = i
 		}
 	}
 
 	fmt.Println()
 
-	if *faceIndex1 > *faceIndex2 {
-		fmt.Println("Bigger card is: ", deck[*faceIndex1].deck.face, " ", deck[*suitIndex1].deck.suit)
+	if faceIndex1 > faceIndex2 {
+		fmt.Println("Bigger card is: ", deck[faceIndex1].deck.face, " ", deck[suitIndex1].deck.suit)
 
-	} else if *faceIndex1 < *faceIndex2 {
-		fmt.Println("Bigger card is: ", deck[*faceIndex2].deck.face, " ", deck[*suitIndex2].deck.suit)
+	} else if faceIndex1 < faceIndex2 {
+		fmt.Println("Bigger card is: ", deck[faceIndex2].deck.face, " ", deck[suitIndex2].deck.suit)
 
-	} else if faceIndex1 == faceIndex2 && *suitIndex1 > *suitIndex2 {
-		fmt.Println("Bigger card is: ", deck[*faceIndex1].deck.face, " ", deck[*suitIndex2].deck.suit)
+	} else if faceIndex1 == faceIndex2 && suitIndex1 > suitIndex2 {
+		fmt.Println("Bigger card is: ", deck[faceIndex1].deck.face, " ", deck[suitIndex2].deck.suit)
 
-	} else if faceIndex1 == faceIndex2 && *suitIndex1 < *suitIndex2 {
-		fmt.Println("Bigger card is: ", deck[*faceIndex2].deck.face, " ", deck[*suitIndex1].deck.suit)
+	} else if faceIndex1 == faceIndex2 && suitIndex1 < suitIndex2 {
+		fmt.Println("Bigger card is: ", deck[faceIndex2].deck.face, " ", deck[suitIndex1].deck.suit)
 	}
 
 	if faceIndex1 == faceIndex2 && suitIndex1 == suitIndex2 {
 		fmt.Println("Cards are equal")
 	}
 }
-func maxCard(compare func(*Card, *Card, *[]DeckOfCards), cards *[]DeckOfCards, firstCard *Card, secondCard *Card) {
+func maxCard(cards []DeckOfCards) {
 
-	compare(firstCard, secondCard, cards)
+	fmt.Println("Max card from the slice: ", cards[len(cards)-1])
 }
 func main() {
 
-	var first *Card
-	var second *Card
-
-	d := &[52]DeckOfCards{}
+	d := make([]DeckOfCards, 52)
 	fmt.Println("Printing deck of all cards with face and suit")
 
 	for i := range d {
-		d[i].deck = &Card{face: faces[i%13], suit: suits[i/13]}
+		d[i].deck = Card{face: faces[i%13], suit: suits[i/13]}
+
+		fmt.Print(d[i].deck)
+
+		if i%4 == 0 && i > 0 {
+			fmt.Println()
+		}
 	}
 
-	//deckSlice := d[5:25]
+	deckSlice := d[5:25]
 	fmt.Println()
 
 	fmt.Println()
 	fmt.Println("Slice from index 5 to 25")
-	for j := range d {
-		fmt.Print(d[j].deck.face, "-", d[j].deck.suit, "  ")
+	for j := range deckSlice {
+		fmt.Print(deckSlice[j], " ")
 		if j%4 == 0 && j > 0 {
 			fmt.Println()
 		}
 	}
-	//maxCard(compareCards, d, first, second)
+	fmt.Println()
+	maxCard(deckSlice)
 	fmt.Println()
 
-	compareCards(first, second, d)
 	fmt.Println()
+	compareCards(d)
 
 }
