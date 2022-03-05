42109b517e5b8979e3ad30c259063401637e15e2
Ivan Markovski <sagitariusim@gmail.com>
2022-03-05 19:13:48 +0100

    For review

--- Lecture_8_Task_2/main.go
+++ Lecture_8_Task_2/main.go
@@ -28,27 +28,27 @@ func (c Circle) Area() float64 {
 	return math.Pow(c.radius, 2) * math.Pi
 }
 
-func (s Shapes) Area() Shapes {
+func (s Shapes) Area() {
 	s[0] = Square{width: 7, height: 4}
 	s[1] = Square{width: 8, height: 3}
 	s[2] = Circle{radius: 5}
 	s[3] = Circle{radius: 14}
 
-	return s
+	for i := range s {
+		fmt.Println(s[i].Area())
+	}
+
 }
 
 func (s Shapes) LargestArea() {
 
-	for i := range s.Area() {
-		fmt.Println(s.Area()[i])
-	}
 }
 
 func main() {
 
 	var sh Shapes
 	sh = Shapes{}
-	fmt.Println(sh.Area())
+	sh.Area()
 	sh.LargestArea()
 
 }
