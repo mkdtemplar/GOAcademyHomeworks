e4333d52d61e757ec27df05b36f6e50c4f3e969d
Ivan Markovski <sagitariusim@gmail.com>
2022-03-12 17:14:42 +0100

    For review

--- Lecture_9_Task_2/main.go
+++ Lecture_9_Task_2/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+type Action func() error
+
+func SafeExec(a Action) Action {
+	return func() error {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Errorf("Recovered in function: %s", r)
+
+			}
+		}()
+		panic("BOOM")
+	}
+}
+
 func main() {
 
 }
