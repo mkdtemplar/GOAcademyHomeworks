48ee6d2b69e41aad503e3ba89ac9007472155c37
Ivan Markovski <sagitariusim@gmail.com>
2022-03-12 21:46:46 +0100

    For review

--- Lecture_9_Task_2/main.go
+++ Lecture_9_Task_2/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 type Action func() error
 
@@ -8,14 +12,27 @@ func SafeExec(a Action) Action {
 	return func() error {
 		defer func() {
 			if r := recover(); r != nil {
-				fmt.Errorf("Recovered in function: %s", r)
+				fmt.Errorf("recovered in function: %s", r)
 
 			}
 		}()
 		panic("BOOM")
 	}
+	return a
+}
+
+func Function() error {
+
+	return errors.New("error")
+
 }
 
 func main() {
 
+	var ar Action
+
+	ar = func() float64 {
+		return math.Sqrt(-1)
+	}
+	SafeExec(ar)
 }
