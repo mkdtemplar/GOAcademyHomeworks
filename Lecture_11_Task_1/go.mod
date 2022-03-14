fdc7d090b51347fb59aebeab152785422a05b83a
Ivan Markovski <sagitariusim@gmail.com>
2022-03-14 13:16:50 +0100

    For review

---
+++ Lecture_11_Task_1/go.mod
@@ -0,0 +1,3 @@
+module Lecture_11_Task_1
+
+go 1.17
--- Lecture_11_Task_1/main.go
+++ Lecture_11_Task_1/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+type Concurency struct {
+	sync.WaitGroup
+	sync.Mutex
+}
+
+func processEven(inputs []int) chan int {
+
+	even := make(chan int)
+	for i := range inputs {
+		if inputs[i]%2 == 0 {
+			return even
+		}
+	}
+
+	return nil
+
+}
+func processOdd(inputs []int) chan int {
+
+	odd := make(chan int)
+	for i := range inputs {
+		if inputs[i]%2 != 0 {
+			return odd
+		}
+	}
+	return nil
+}
+
 func main() {
+
+	inputs := []int{1, 17, 34, 56, 2, 8}
+	cp := &Concurency{}
+
+	cp.Add(2)
+
+	go func() {
+		fmt.Println(processEven(inputs))
+		cp.Done()
+	}()
+
+	go func() {
+		fmt.Println(processOdd(inputs))
+		cp.Done()
+	}()
+
+	cp.Wait()
 
 }
