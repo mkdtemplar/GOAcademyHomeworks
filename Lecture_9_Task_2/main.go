1b675d68504ebf3c4166345071c83e499f9a49e5
Ivan Markovski <sagitariusim@gmail.com>
2022-03-14 04:19:05 +0100

    For review

--- Lecture_9_Task_2/main.go
+++ Lecture_9_Task_2/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+type Action func() error
+
+func (a Action) Error() string {
+	var aa float64
+	scan, err := fmt.Scan(&aa)
+	if err != nil {
+		fmt.Println(math.Sqrt(float64(scan)))
+		return "Error"
+	} else {
+
+		return "OK"
+	}
+	panic("implement me")
+}
+
+func (a Action) Divide() float64 {
+
+	return 1 / 0
+}
+
+func SafeExec(a Action) Action {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Errorf("recovered in function: %s", r)
+
+		}
+	}()
+
+	a.Divide()
+	panic("BOOM")
+}
+
 func main() {
+
+	var ar Action
+	fmt.Println(SafeExec(ar).Divide())
 
 }
