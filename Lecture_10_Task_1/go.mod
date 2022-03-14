8dc8ae1adf857d6200b545c8bb21542dba7058d6
Ivan Markovski <sagitariusim@gmail.com>
2022-03-14 01:54:12 +0100

    For review

---
+++ Lecture_10_Task_1/go.mod
@@ -0,0 +1,3 @@
+module Lecture_10_Task_1
+
+go 1.17
--- Lecture_10_Task_1/main.go
+++ Lecture_10_Task_1/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type ConcurrentPrinter struct {
+	sync.WaitGroup
+	sync.Mutex
+}
+
+func (cp *ConcurrentPrinter) PrintFoo(times int) {
+
+	for i := 0; i < times; i++ {
+		cp.Lock()
+		fmt.Print("foo")
+		defer cp.Unlock()
+	}
+
+}
+
+func (cp *ConcurrentPrinter) PrintBar(times int) {
+
+	time.Sleep(10 * time.Millisecond)
+	for i := 0; i < times; i++ {
+		cp.Lock()
+		fmt.Print("bar")
+		defer cp.Unlock()
+	}
+
+}
+
 func main() {
 
+	times := 10
+	cp := &ConcurrentPrinter{}
+
+	cp.Add(2)
+
+	go func() {
+		cp.PrintFoo(times)
+		cp.Done()
+	}()
+
+	go func() {
+		cp.PrintBar(times)
+		cp.Done()
+	}()
+
+	cp.Wait()
 }
