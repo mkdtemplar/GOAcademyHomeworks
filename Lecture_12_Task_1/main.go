3afb4af61368eb74331917aa3d4d03d1e70085ab
Ivan Markovski <sagitariusim@gmail.com>
2022-03-16 21:33:27 +0100

    For review

--- Lecture_12_Task_1/main.go
+++ Lecture_12_Task_1/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateThrottled(data string, bufferLimit int, clearInterval time.Duration) <-chan string {
+	channel := make(chan string)
+	clearInterval = time.Duration(rand.Intn(1e3))
+	//var wg sync.WaitGroup
+	//wg.Add(bufferLimit)
+	for i := 0; i < bufferLimit; i++ {
+		go func() {
+			channel <- data
+			//wg.Done()
+			time.Sleep(clearInterval * time.Millisecond)
+			close(channel)
+		}()
+	}
+	
+	return channel
+}
 func main() {
 
+	clearInterval := time.Duration(rand.Intn(1e3))
+	c := generateThrottled("Data string", 2, clearInterval)
+
+	for i := range c{
+		fmt.Println(i)
+	}
 }
