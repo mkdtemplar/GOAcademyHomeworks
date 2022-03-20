52e6c1debe3085183116181214f9d97da5119449
Ivan Markovski <sagitariusim@gmail.com>
2022-03-21 00:04:39 +0100

    For review

--- Lecture_13_Task_1/main.go
+++ Lecture_13_Task_1/main.go
@@ -45,19 +45,23 @@ func (bc *BufferedContext) Done() <-chan struct{} {
 	   a) the emebdded context times out
 	   b) the buffer gets filled
 	*/
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*200)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
 	bc = &BufferedContext{ctx}
 
-	return bc.Context.Done()
+	return bc.Done()
 }
 func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
 	/* This function serves for executing the test */
 	/* Implement the rest */
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*200)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 	chanel := make(chan string)
+
+	for i := 0; i < len(chanel); i++ {
+		chanel <- "bar"
+	}
 
 	fn(ctx, chanel)
 }
@@ -77,50 +81,3 @@ func main() {
 		}
 	})
 }
-
-/*
-unc NewBufferedContext(timeout time.Duration, bufferSize int) *BufferedContext {
-
-    ctx, cancel := context.WithTimeout(context.Background(), timeout)
-
-
-
-    defer cancel()
-
-    bc := &BufferedContext{ctx, 10, timeout}
-
-
-
-    return bc
-
-
-
-}
-
-func (bc *BufferedContext) Done() <-chan struct{} {
-
-
-
-    ctx, cancel := context.WithTimeout(bc, bc.timeout)
-
-    defer cancel()
-
-    return ctx.Done()
-
-}
-
-func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
-
-    ch := make(chan string, bc.buffer)
-
-    for len(ch) == cap(ch) {
-
-        ch <- "bar"
-
-        time.Sleep(time.Millisecond * 200)
-
-    }
-
-    fn(bc.Context, ch)
-}
-*/
