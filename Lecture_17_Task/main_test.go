0128a9c557439c1f02096d40e8c1010a905aebf8
Ivan Markovski <sagitariusim@gmail.com>
2022-04-02 23:01:12 +0200

    For review

--- Lecture_17_Task/main_test.go
+++ Lecture_17_Task/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"testing"
-
 )
 
 type DeckCard struct {
@@ -14,6 +13,14 @@ type DeckCard struct {
 var addResults = []DeckCard{
 	{card1: Card{face: "Deuce", suit: "Clubs"}, card2: Card{face: "Deuce", suit: "Clubs"}, expected: Card{face: "Deuce", suit: "Clubs"}},
 	{card1: Card{face: "Three", suit: "Clubs"}, card2: Card{face: "Three", suit: "Clubs"}, expected: Card{face: "Three", suit: "Clubs"}},
+	{card1: Card{face: "Ace", suit: "Clubs"}, card2: Card{face: "Deuce", suit: "Clubs"}, expected: Card{face: "Ace", suit: "Clubs"}},
+	{card1: Card{face: "Three", suit: "Clubs"}, card2: Card{face: "King", suit: "Clubs"}, expected: Card{face: "King", suit: "Clubs"}},
+	{card1: Card{face: "Six", suit: "Clubs"}, card2: Card{face: "Deuce", suit: "Spades"}, expected: Card{face: "Six", suit: "Clubs"}},
+	{card1: Card{face: "Jack", suit: "Clubs"}, card2: Card{face: "Jack", suit: "Diamonds"}, expected: Card{face: "Jack", suit: "Clubs"}},
+	{card1: Card{face: "Ten", suit: "Clubs"}, card2: Card{face: "Deuce", suit: "Clubs"}, expected: Card{face: "Ten", suit: "Clubs"}},
+	{card1: Card{face: "Seven", suit: "Diamonds"}, card2: Card{face: "Three", suit: "Clubs"}, expected: Card{face: "Seven", suit: "Diamonds"}},
+	{card1: Card{face: "Queen", suit: "Clubs"}, card2: Card{face: "Queen", suit: "Spades"}, expected: Card{face: "Queen", suit: "Clubs"}},
+	{card1: Card{face: "King", suit: "Clubs"}, card2: Card{face: "Ace", suit: "Diamonds"}, expected: Card{face: "Ace", suit: "Diamonds"}},
 }
 
 func TestInput(t *testing.T) {
