1061da87ab5850769b1c58794e17642f824b847d
Ivan Markovski <sagitariusim@gmail.com>
2022-04-04 12:53:07 +0200

    For review

--- Lecture_18_Task_1/main_test.go
+++ Lecture_18_Task_1/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func Benchmark100PrimesWith0MSSleep(b *testing.B) {
@@ -37,5 +39,28 @@ func Benchmark100PrimesWith5MSSleep(b *testing.B) {
 func Benchmark100GoPrimesWith10MSSleep(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		goPrimesAndSleep(100, 10)
+	}
+}
+
+func BenchmarkGoPrimesWithSleep(b *testing.B) {
+	sleepTimes := []time.Duration{time.Millisecond * 0, time.Millisecond * 5, time.Millisecond * 10}
+	for _, st := range sleepTimes {
+		b.Run(fmt.Sprintf("Benchmark-sleep-time-%d", st), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				goPrimesAndSleep(100, st)
+			}
+		})
+	}
+
+}
+
+func Benchmark100PrimesSleep(b *testing.B) {
+	sleepTimes := []time.Duration{time.Millisecond * 0, time.Millisecond * 5, time.Millisecond * 10}
+	for _, st := range sleepTimes {
+		b.Run(fmt.Sprintf("Benchmark-sleep-time-%d", st), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				primesAndSleep(100, st)
+			}
+		})
 	}
 }
