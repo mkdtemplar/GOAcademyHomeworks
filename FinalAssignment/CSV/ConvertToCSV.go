8d0e855064c45e0180124825dda4047a162b1130
Ivan Markovski <sagitariusim@gmail.com>
2022-05-15 13:59:01 +0200

    For grading

--- FinalAssignment/CSV/ConvertToCSV.go
+++ FinalAssignment/CSV/ConvertToCSV.go
@@ -18,7 +18,7 @@ func ReadListRow(c *gin.Context) {
 	csvWriter := csv.NewWriter(csvFile)
 	var list []models.Lists
 	models.DB.Find(&list)
-
+	_ = csvWriter.Write([]string{"Id", "Name"})
 	for i := 0; i < len(list); i++ {
 		var row []string
 		row = append(row, strconv.Itoa(int(list[i].Id)))
