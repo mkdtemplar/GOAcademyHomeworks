54498dbc6a577f0c369a144521007c1e4ef085da
Ivan Markovski <sagitariusim@gmail.com>
2022-05-20 16:05:24 +0200

    For grading

--- FinalAssignment/cmd/gin/main.go
+++ FinalAssignment/cmd/gin/main.go
@@ -4,8 +4,11 @@ import (
 	models "FinalAssignment/Repository/DatabaseContext"
 	routers "FinalAssignment/RoutersSetup"
 	"FinalAssignment/cmd"
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -14,7 +17,27 @@ func main() {
 
 	router := routers.Setup()
 
+	exit := make(chan struct{}, 1)
+	// start the server
+	go func() {
+		log.Fatal(http.ListenAndServe(":3000", cmd.CreateCommonMux(router)))
+		exit <- struct{}{}
+	}()
+
+	var err error
+	switch runtime.GOOS {
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://localhost:3000/swagger").Start()
+	case "darwin":
+		err = exec.Command("open", "http://localhost:3000/swagger").Start()
+	case "linux":
+		err = exec.Command("xdg-open", "http://localhost:3000/swagger").Start()
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	<-exit
+
 	// Do not touch this line!
-	log.Fatal(http.ListenAndServe(":3000", cmd.CreateCommonMux(router)))
 
 }
